8f56fec41993b94bde904175503d7f1c164f6944
Manuele Macchia <macchiamanuele@gmail.com>
2021-09-11 18:39:04 +0200

    VGG16 freezable feature extractor

--- src/network/vgg16.py
+++ src/network/vgg16.py
@@ -4,10 +4,22 @@ from tensorflow.keras.applications import vgg16
 
 def VGG16(n_classes,
           input_shape=(224, 224, 3),
+          dropout_rate=0.5,
           load_imagenet=True,
-          dropout_rate=0.5):
-    """
-    VGG16 model for training and inference based on the original paper.
+          feature_extractor_trainable=False):
+    """VGG16 model for training and inference, based on the original paper.
+
+    Args:
+        n_classes: Number of output classes to classify images into.
+        input_shape: Shape of the input.
+        dropout_rate: Rate of dropout for fully connected dropout layers.
+        load_imagenet: If True, load imagenet weights for the feature extractor.
+            Note that the dense classifier block is always randomly initialized.
+        feature_extractor_trainable: If False, freeze the feature extractor
+            layers. Otherwise, set them to trainable layers.
+
+    Returns:
+        model: VGG16 model with classifier.
     """
 
     model_base = vgg16.VGG16(
@@ -15,6 +27,9 @@ def VGG16(n_classes,
         weights='imagenet' if load_imagenet else None,
         input_shape=input_shape
     )
+
+    if not feature_extractor_trainable:
+        model_base.trainable = False
 
     x = model_base.output
 
