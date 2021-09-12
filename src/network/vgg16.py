7d4f8abea5ac77bc653a88eb84a94a069eab01c5
Manuele Macchia <macchiamanuele@gmail.com>
2021-09-12 09:37:37 +0200

    VGG16 variable number of dense units

--- src/network/vgg16.py
+++ src/network/vgg16.py
@@ -5,6 +5,7 @@ from tensorflow.keras.applications import vgg16
 def VGG16(n_classes,
           input_shape=(224, 224, 3),
           dropout_rate=0.5,
+          dense_units=4096,
           load_imagenet=True,
           feature_extractor_trainable=False):
     """VGG16 model for training and inference, based on the original paper.
@@ -13,6 +14,7 @@ def VGG16(n_classes,
         n_classes: Number of output classes to classify images into.
         input_shape: Shape of the input.
         dropout_rate: Rate of dropout for fully connected dropout layers.
+        dense_units: Number of units of fully connected layers.
         load_imagenet: If True, load imagenet weights for the feature extractor.
             Note that the dense classifier block is always randomly initialized.
         feature_extractor_trainable: If False, freeze the feature extractor
@@ -36,9 +38,9 @@ def VGG16(n_classes,
     # Dense block
     x = layers.Flatten(name='flatten')(x)
 
-    x = layers.Dense(4096, activation='relu', name='fc1')(x)
+    x = layers.Dense(dense_units, activation='relu', name='fc1')(x)
     x = layers.Dropout(dropout_rate, name='fc1_dropout')(x)
-    x = layers.Dense(4096, activation='relu', name='fc2')(x)
+    x = layers.Dense(dense_units, activation='relu', name='fc2')(x)
     x = layers.Dropout(dropout_rate, name='fc2_dropout')(x)
 
     output = layers.Dense(n_classes, activation='softmax', name='predictions')(x)
