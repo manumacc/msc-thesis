89568489cf39c7bfa0c228b09f52d080747ea4c0
Manuele Macchia <macchiamanuele@gmail.com>
2021-09-18 13:44:19 +0200

    Query strategy may use a preprocessing function

--- src/qs.py
+++ src/qs.py
@@ -1,6 +1,7 @@
 class QueryStrategy:
-    def __init__(self, model):
+    def __init__(self, model, preprocess_input_fn=None):
         self.model = model
+        self.preprocess_input_fn = preprocess_input_fn
 
     def __call__(self, X_pool, n_query_instances, seed=None):
         raise NotImplementedError("Can't call a base class")
